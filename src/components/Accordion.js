36c5f55e3587aa960f8754a01faa0a4cb7519d52
Callan <cerveauxallanjean@gmail.com>
2022-02-23 15:55:07 +0100

    Rename variable

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function Accordion({status, content, title, handleClick, id}){
 
-    const onPress = () => handleClick(id);
+    const trigger = () => handleClick(id);
     const arrow = () => status ? (<span className='accordion-hearder--icon'>&and;</span>) : (<span className='accordion-hearder--icon'>&or;</span>)
 
     return (
       <article className='accordion'>
-        <div className='accordion-header' onClick={onPress}>
+        <div className='accordion-header' onClick={trigger}>
           <h2>{title}</h2>
           {arrow()}
         </div>
