eaca2d7ddc14b4b422ff8d8edfe76209cc6ec7c7
Callan <cerveauxallanjean@gmail.com>
2022-02-23 19:20:07 +0100

    Fix LodgingsToLodging and style this

--- src/components/LodgingsToLodging.js
+++ src/components/LodgingsToLodging.js
@@ -3,14 +3,14 @@ import { useNavigate } from 'react-router-dom';
 
 export default function LodgingsToLodging({ lodgings }) {
   const navigate = useNavigate()
-
+  const navigateToLodgingById = (id) => () => navigate(`/lodging/${id}`)
+  
   return (
     <section className='logdings'>
       {
         lodgings?.map(l => (
-          <article className='card' onClick={() => navigate(`/lodging/${l.id}`)} key={`lodging-${l.id}`}>
-            <h2 className='card-title'>{l?.title}</h2>
-            <img className='card-image' src={l?.cover} alt={ `${l?.title} - ${l?.location}` }/>
+          <article className='card' style={{background: `url(${l.cover})`}} onClick={navigateToLodgingById(l.id)} key={`lodging-${l.id}`}>
+            <h2 className='card-title'>{l.title}</h2>
           </article>
         )) || null
       }
