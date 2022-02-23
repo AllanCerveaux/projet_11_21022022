ede1e1e06828e5ced9b7861173a3c851e17a5e4a
Callan <cerveauxallanjean@gmail.com>
2022-02-23 21:47:25 +0100

    Style rating

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -3,6 +3,6 @@ import { IdGenerator } from '../lib/IdGenerator'
 
 export default function Rating({ isActive }) {
   return (
-    <span key={IdGenerator()} className={`icon ${ isActive ? 'active' : '' }`}>&#9733;</span>
+    <span key={IdGenerator()} className={`icon-rating ${ isActive ? 'active' : '' }`}>&#9733;</span>
   )
 }
