1ff24cf86d9ca728f8a7151f3c38648e99d223a1
Callan <cerveauxallanjean@gmail.com>
2022-02-23 20:25:24 +0100

    Style error component

--- src/containers/Errors.js
+++ src/containers/Errors.js
@@ -4,8 +4,10 @@ import { Link } from "react-router-dom";
 export default function Errors(){
     return (
       <section className="error">
-        <h1>404</h1>
-        <p>La page que vous demandez n'existe pas</p>
+        <div className='error-message'>
+          <h1 className='error-code'>404</h1>
+          <p>La page que vous demandez n'existe pas</p>
+        </div>
         <Link to="">Retourner sur la page d'acceuil</Link>
       </section>
     )
